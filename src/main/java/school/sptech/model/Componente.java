836c6031fe770d2cc63597f5d9a7253e83676f49
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-12-07 20:41:01 -0300

    alteracao nas models

--- src/main/java/school/sptech/model/Componente.java
+++ src/main/java/school/sptech/model/Componente.java
@@ -1,50 +1,17 @@
 package school.sptech.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public abstract class Componente {
     private Integer id;
     private String tipo;
-    private Double valor;
-    private LocalDateTime dataHora;
+    private List<Registro> registros;
 
-    public Componente(Integer id, String tipo, Double valor, LocalDateTime dataHora) {
+    public Componente(Integer id, String tipo, List<Registro> registros) {
         this.id = id;
         this.tipo = tipo;
-        this.valor = valor;
-        this.dataHora = dataHora;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
-    }
-
-    public LocalDateTime getDataHora() {
-        return dataHora;
-    }
-
-    public void setDataHora(LocalDateTime dataHora) {
-        this.dataHora = dataHora;
+        this.registros = registros;
     }
 
     @Override
@@ -52,8 +19,7 @@ public abstract class Componente {
         final StringBuffer sb = new StringBuffer("Componente{");
         sb.append("id=").append(id);
         sb.append(", tipo='").append(tipo).append('\'');
-        sb.append(", valor=").append(valor);
-        sb.append(", dataHora=").append(dataHora);
+        sb.append(", registros=").append(registros);
         sb.append('}');
         return sb.toString();
     }
--- src/main/java/school/sptech/model/Registro.java
+++ src/main/java/school/sptech/model/Registro.java
@@ -1,4 +1,13 @@
 package school.sptech.model;
 
+import java.time.LocalDateTime;
+
 public class Registro {
+    private Double valor;
+    private LocalDateTime dataHora;
+
+    public Registro(Double valor, LocalDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
 }
